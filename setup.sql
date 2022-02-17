9f616761de491e15a90dc85480c0730e9e04faaf
noellekh <63249943+noellekh@users.noreply.github.com>
2022-02-17 21:05:35 +0100

    correction syntax db

--- setup.sql
+++ setup.sql
@@ -1,51 +1,45 @@
-CREATE TABLE IF NOT EXISTS `users` (
-  `id_user` int(11) NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(50) NOT NULL,
-  `user_surname` varchar(50) NOT NULL,
-  'user_birth' date NOT NULL,
-  'user_email' varchar(150) NOT NULL UNIQUE,
-  'user_phone' varchar(15) NOT NULL,
-  'user_sex' varchar(1) NOT NULL,
-  'user_street' varchar(250) NOT NULL,
-  'postal' integer NOT NULL, 
-  'newsletter' integer
+CREATE TABLE cities(
+  id_postal int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  postal int(5) NOT NULL,
+  city varchar (50) NOT NULL
+);
+
+CREATE TABLE users (
+  id_user int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_nom varchar(50) NOT NULL,
+  user_surname varchar(50) NOT NULL,
+  user_birth date NOT NULL,
+  user_email varchar(150) NOT NULL UNIQUE,
+  user_phone varchar(15) NOT NULL,
+  user_sex varchar(1) NOT NULL,
+  user_street varchar(250) NOT NULL,
+  postal int(5) NOT NULL, 
+  newsletter int(1),
   
-  CONSTRAINT pk_users PRIMARY KEY (id_user),
-  CONSTRAINT fk_cities_users FOREIGN KEY (postal) REFERENCES cities(postal)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
-
-
-CREATE TABLE ID NOT EXISTS 'cities'(
-  'postal' integer NOT NULL,
-  'city' varchar (50) NOT NULL
-
-  CONSTRAINT pk_cities PRIMARY KEY (postal)
+  CONSTRAINT fk_cities_users FOREIGN KEY (postal) REFERENCES cities(id_postal)
 );
 
-CREATE TABLE 'scores'(
-  'id_user' integer(11) NOT NULL REFERENCES users,
-  'id_score' integer NOT NULL AUTO_INCREMENT,
-  'score' integer NOT NULL
 
-  CONSTRAINT pk_scores PRIMARY KEY (id_score),
+CREATE TABLE scores(
+  id_user integer(11) NOT NULL,
+  id_score integer NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  score integer NOT NULL,
+
   CONSTRAINT pk_users_scores FOREIGN KEY (id_user) REFERENCES users(id_user)
+);
+
+CREATE TABLE product (
+  id_product int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  product_name varchar (50) NOT NULL,
+  product_price varchar(10) NOT NULL,
+  product_description varchar (100) NOT NULL
 
 );
 
-CREATE TABLE 'product' (
-  'id_product' int(11) NOT NULL AUTO_INCREMENT,
-  'product_name' varchar (50) NOT NULL,
-  'product_price' varchar(10) NOT NULL,
-  'product_description' varchar (100) NOT NULL,
+CREATE TABLE agenda_client(
+  id_user integer (11) NOT NULL,
+  id_agenda_client integer (11) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  id_date date NOT NULL,
 
-  CONSTRAINT pk_product PRIMARY KEY(id_product)
-);
-
-CREATE TABLE 'agenda_client'(
-  'id_user' integer (11) NOT NULL REFERENCES users,
-  'id_agenda_client' integer (11) NOT NULL,
-  'date' date NOT NULL
-
-  CONSTRAINT pk_agenda_client PRIMARY KEY (id_agenda_client),
   CONSTRAINT pk_users_agenda FOREIGN KEY (id_user) REFERENCES users(id_user)
-)
+);
