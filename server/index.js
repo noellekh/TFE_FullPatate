21a59718d81691cc5919a199784851a0934bdd53
noellekh <63249943+noellekh@users.noreply.github.com>
2022-02-18 20:03:53 +0100

    connection a la db + install mysql

--- server/index.js
+++ server/index.js
@@ -1,25 +1,45 @@
 const express = require ('express');
 const mysql = require ('mysql2');
 const cors = require ('cors'); 
-const { application } = require('express');
 const app = express();
 
-const db = mysql.createPool ({
-    host: 'fullpatate',
+const db = mysql.createConnection ({
+    host: 'localhost',
     user: 'balboa',
     password: 'fullpatate',
     database: 'fp_db'
 
+});
+
+db.connect((err)=>{
+    if(err){
+        throw err;
+    }
+    console.log('connecté à MSQL...')
+})
+
+app.listen ('3001', () =>{
+    console.log("running on port 3001")
 })
 
 app.use(cors()) // enable cors security headers
 
 
 app.use(express.json())
-app.use(express.urlencoded({extended :true}));
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Full Patate Always !')
 });
 
-app.listen ('3001', () =>{})
+app.get('/api/users', (req, res) => {
+    const SelectQuery = " SELECT * FROM `users`";
+    db.query(SelectQuery, (err, result) => {
+        if (err) throw err;
+        res.send(result)
+        console.log(result)
+    })
+})
+
