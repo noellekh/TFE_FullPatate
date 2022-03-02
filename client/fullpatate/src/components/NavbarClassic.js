5857f35784c6d9684664490d71be5f9e4f939392
noellekh <63249943+noellekh@users.noreply.github.com>
2022-03-02 20:25:06 +0100

    NavbarClassic responsive

---
+++ client/fullpatate/src/components/NavbarClassic.js
@@ -0,0 +1,52 @@
+import React, {useState} from "react";
+import {FaFacebookSquare, FaInstagramSquare} from "react-icons/fa";
+import '../css/NavbarClassic.css';
+import {GiHamburgerMenu} from "react-icons/gi";
+import { NavLink } from "react-router-dom";
+
+const NavbarClassic =()=>{
+    const [showMediaIcons, setShowMediaIcons] = useState(false);
+
+    
+    return (
+        <>
+
+            <nav className="navbar-classic">
+                <div className="logo-fp">
+                    <h2>Full Patate</h2>
+
+                </div>
+
+                <div className={showMediaIcons ? "menu-classic mobile-menu-classic": "menu-classic"}>
+                    <ul>
+                        <li><a href="#">Accueil</a></li>
+                        <li><a href="#">E-shop</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Inscription</a></li>
+                        <li><a href="#">Connexion</a></li>
+  
+
+                        
+                    </ul>
+                </div>
+
+                <div className="social-media">
+                    <ul className="social-media-desktop">
+                        <li><a href ='#'><FaFacebookSquare /></a></li>
+                        <li><a href ='#'><FaInstagramSquare /></a></li>
+                    </ul>
+                    <div className="hamburger-menu">
+                        <a href="#" onClick={()=> setShowMediaIcons(!showMediaIcons)}>
+                            <GiHamburgerMenu />
+                        </a>
+                    
+                    </div>
+
+                </div>
+            </nav>
+        </>
+
+    )
+}
+
+export default NavbarClassic;
